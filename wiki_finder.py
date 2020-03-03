2f5be36122dca6e82c23434a0b576f37558f01e3
TheUhrMacher <theuhrmacher@protonmail.com>
2020-03-03 00:19:59 -0500

    Added a simple Wikipedia page finder

---
+++ wiki_finder.py
@@ -0,0 +1,11 @@
+# Basic packages to get the scirpt up and running
+import webbrowser, wikipedia, sys
+
+def main(argv):
+	# Ask the user for a page to query
+	page_search = wikipedia.page(input("Page Search: "))
+	# Open a web browser to the page
+	webbrowser.open_new(page_search.url)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
